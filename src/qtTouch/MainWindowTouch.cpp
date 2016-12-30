e1d96967db889cf1a6aeef9166f2bb80964252a6
Teseo Schneider <teseo@bluewin.ch>
2016-12-30 16:58:21 +0100

    rotated the buttons

--- src/qtTouch/MainWindowTouch.cpp
+++ src/qtTouch/MainWindowTouch.cpp
@@ -36,7 +36,7 @@ QMainWindow(parent), _ui(new Ui::MainWindowTouch), _printer(QPrinter::HighResolu
 
 	_ui->setupUi(this);
 // #ifndef DEBUG
-	   showFullScreen();
+	   // showFullScreen();
 // #endif
 
 	QList<QPrinterInfo> infos = QPrinterInfo::availablePrinters();
@@ -118,20 +118,30 @@ void MainWindowTouch::resizeEvent(QResizeEvent * event)
 {
 	const QSize &size = this->size();
 
-	const int voffset = 20;
 	const int h = float(size.width()) / PaperConstants::PAGE_WIDTH()*PaperConstants::PAGE_HEIGHT();
 	const int editH = size.height() - h;
 	_ui->mainView->resize(size.width(), h);
 	_ui->mainView->move(0, editH);
 
-	_ui->webcamImg->move(size.width() / 2 - LARGE_BTN_SIZE / 2, size.height() - LARGE_BTN_SIZE-voffset);
+	// const int voffset = 20;
+	// _ui->webcamImg->move(size.width() / 2 - LARGE_BTN_SIZE / 2, size.height() - LARGE_BTN_SIZE-voffset);
 
-	_ui->print->move(size.width() - SMALL_BTN_SIZE * 3 - 2 * BTN_OFFSET, size.height() - SMALL_BTN_SIZE-voffset);
-	_ui->sendEmail->move(size.width() - SMALL_BTN_SIZE * 2 - BTN_OFFSET, size.height() - SMALL_BTN_SIZE-voffset);
-	_ui->facebookShare->move(size.width() - SMALL_BTN_SIZE, size.height() - SMALL_BTN_SIZE-voffset);
+	// _ui->print->move(size.width() - SMALL_BTN_SIZE * 3 - 2 * BTN_OFFSET, size.height() - SMALL_BTN_SIZE-voffset);
+	// _ui->sendEmail->move(size.width() - SMALL_BTN_SIZE * 2 - BTN_OFFSET, size.height() - SMALL_BTN_SIZE-voffset);
+	// _ui->facebookShare->move(size.width() - SMALL_BTN_SIZE, size.height() - SMALL_BTN_SIZE-voffset);
+	// _ui->showHidePoly->move(0, size.height() - SMALL_BTN_SIZE-voffset);
 
 
-	_ui->showHidePoly->move(0, size.height() - SMALL_BTN_SIZE-voffset);
+
+	const int voffset = 10;
+	const float offset = size.height() /4.0f;
+
+	_ui->webcamImg->move(size.width() - LARGE_BTN_SIZE - voffset, size.height() / 2 - offset);
+
+	_ui->print->move(		 size.width() - SMALL_BTN_SIZE - voffset, size.height() - SMALL_BTN_SIZE * 3 - 2 * BTN_OFFSET - offset);
+	_ui->sendEmail->move(	 size.width() - SMALL_BTN_SIZE - voffset, size.height() - SMALL_BTN_SIZE * 2 - BTN_OFFSET - offset);
+	_ui->facebookShare->move(size.width() - SMALL_BTN_SIZE - voffset, size.height() - SMALL_BTN_SIZE - offset);
+	_ui->showHidePoly->move( size.width() - SMALL_BTN_SIZE - voffset, size.height() + SMALL_BTN_SIZE + BTN_OFFSET - offset);
 
 	{
 		const int logoH = size.width() / 2000.0f*382.0f;
